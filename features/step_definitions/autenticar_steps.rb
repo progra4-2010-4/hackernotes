6a17075e65fa91bca82c4c3eaa22828b8a02a180
lfborjas <luis.borjas@escolarea.com>
2010-11-03 22:35:05 -0600

    lista la primera feature

--- features/step_definitions/autenticar_steps.rb
+++ features/step_definitions/autenticar_steps.rb
@@ -4,7 +4,7 @@ end
 
 
 Dado /^que estoy autenticado con el correo "([^"]*)" y la contraseña "([^"]*)"$/ do | email , password |
- User.new(:email=>email, :password=>password) 
+ User.new(:email=>email, :password=>password).save! 
 
  Cuando %{voy a login}
  Y %{completo "Email" con "#{email}"}
